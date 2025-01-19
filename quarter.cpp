51c8da9980e762aa95f27098e96e6676bc8cc801
Ivabek33 <tiwanski@interia.pl>
2025-01-19 20:09:39 +0100

    Dodanie nagłówka klasy Quarter z metodami zarządzania punktami danych

---
+++ quarter.cpp
@@ -0,0 +1,11 @@
+#include "Quarter.h"
+
+void Quarter::addDataPoint(DataPoint* point) {
+    dataPoints.push_back(point);
+}
+
+void Quarter::sortData() {
+    std::sort(dataPoints.begin(), dataPoints.end(), [](DataPoint* a, DataPoint* b) {
+        return a->dateTime < b->dateTime;
+        });
+}
