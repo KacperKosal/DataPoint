f2ea4ec004d0eeefc36b067ea3f11a8dbb4b2abd
Ivabek33 <tiwanski@interia.pl>
2025-01-27 11:35:20 +0100

    .

--- quarter.cpp
+++ quarter.cpp
@@ -1,12 +1,14 @@
 #include "Quarter.h"
 
-void Quarter::addDataPoint(DataPoint* point) {
+void Quarter::addDataPoint(DataPoint* point) 
+{
     dataPoints.push_back(point);
 }
 
 void Quarter::sortData() 
 {
-    std::sort(dataPoints.begin(), dataPoints.end(), [](DataPoint* a, DataPoint* b) {
+    std::sort(dataPoints.begin(), dataPoints.end(), [](DataPoint* a, DataPoint* b) 
+        {
         return a->dateTime < b->dateTime;
         });
 }
